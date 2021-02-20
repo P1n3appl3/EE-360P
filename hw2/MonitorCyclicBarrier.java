77db134eed65d14a2b2e4b0f66a93056da933425
Joseph Ryan <josephryan3.14@gmail.com>
2021-02-20 14:37:15 -0600

    Finish 1b

--- hw2/MonitorCyclicBarrier.java
+++ hw2/MonitorCyclicBarrier.java
@@ -1,10 +1,45 @@
 public class MonitorCyclicBarrier {
-    public MonitorCyclicBarrier(int parties) {}
+    int n, in, out;
+    Object canEnter;
+    Object canExit;
+    public MonitorCyclicBarrier(int parties) {
+        out = n = parties;
+        in = 0;
+        canEnter = new Object();
+        canExit = new Object();
+    }
 
     public int await() throws InterruptedException {
-        int index = 0;
+        final int index;
+        final long threadNum = Thread.currentThread().getId();
+        synchronized (canEnter) {
+            while (out < n) {
+                // System.out.println("Thread " + threadNum + " waiting on " +
+                //                    (n - out) + " threads to leave");
+                canEnter.wait();
+            }
+        }
 
-        // you need to write this code
+        synchronized (canExit) {
+            index = in++;
+            // System.out.println("Thread " + threadNum + " is index " + index)
+            while (in < n) { canExit.wait(); }
+            if (out == n) {
+                // System.out.println(index + " is the first one out");
+                out = 0;
+            }
+            canExit.notifyAll();
+        }
+
+        synchronized (canEnter) {
+            ++out;
+            // System.out.println(index + " leaving");
+            if (out == n) {
+                in = 0;
+                canEnter.notifyAll();
+                // System.out.println(index + " is the last one out");
+            }
+        }
         return index;
     }
 }
