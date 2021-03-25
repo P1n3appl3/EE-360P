95587fd49010010b28762890fd4abd66c93358b9
jspspike <jspspike@gmail.com>
2021-03-24 21:15:34 -0500

    Started on client code

--- hw3/BookClient.java
+++ hw3/BookClient.java
@@ -1,13 +1,45 @@
 import java.io.*;
 import java.util.*;
 import java.util.Scanner;
-public class BookClient {
-    public static void main(String[] args) {
-        String hostAddress;
-        final int tcpPort = 7000;
-        final int udpPort = 8000;
-        final int clientId;
 
+import java.net.*;
+
+public class BookClient {
+    static boolean tcp = false;
+    static DatagramSocket udpSocket;
+    static Socket tcpSocket;
+    static int clientId;
+
+    static String hostAddress;
+
+    final static int tcpPort = 7000;
+    final static int udpPort = 8000;
+
+    static void sendData(String data) throws IOException {
+        if (tcp) {
+            PrintStream out = new PrintStream(tcpSocket.getOutputStream());
+            out.println(data);
+        } else {
+            InetAddress address = InetAddress.getByName(hostAddress);
+            DatagramPacket packet = new DatagramPacket(data.getBytes("UTF-8"), data.getBytes("UTF-8").length, address, udpPort);
+            udpSocket.send(packet);
+        }
+    }
+
+    static String getData() throws IOException {
+        if (tcp) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(tcpSocket.getInputStream()));
+            return in.readLine();
+        } else {
+            byte[] data = new byte[1<<15];
+            InetAddress address = InetAddress.getByName(hostAddress);
+            DatagramPacket packet = new DatagramPacket(data, data.length, address, udpPort);
+            udpSocket.receive(packet);
+            return new String(data, "UTF-8");
+        }
+    }
+
+    public static void main(String[] args) throws SocketException, FileNotFoundException, IOException {
         if (args.length != 2) {
             System.out.println(
                 "ERROR: Provide 2 arguments: commandFile, clientId");
@@ -21,41 +53,55 @@ public class BookClient {
         clientId = Integer.parseInt(args[1]);
         hostAddress = "localhost";
 
-        try {
-            Scanner sc = new Scanner(new FileReader(commandFile));
+        udpSocket = new DatagramSocket();
 
-            while (sc.hasNextLine()) {
-                String cmd = sc.nextLine();
-                String[] tokens = cmd.split(" ");
+        Scanner sc = new Scanner(new FileReader(commandFile));
+        PrintWriter writer = new PrintWriter("out_" + clientId, "UTF-8");
 
-                if (tokens[0].equals("setmode")) {
-                    char mode = tokens[1].charAt(0);
-                    System.out.println("SetMode: " + mode);
-                    // TODO
-                } else if (tokens[0].equals("borrow")) {
-                    String student = tokens[1];
-                    String book = cmd.substring(cmd.indexOf('"') - 1);
-                    System.out.println("Borrow: Student=" + student +
-                                       " Book=" + book);
-                    // TODO
-                } else if (tokens[0].equals("return")) {
-                    int id = Integer.parseInt(tokens[1]);
-                    System.out.println("Return: " + id);
-                    // TODO
-                } else if (tokens[0].equals("inventory")) {
-                    System.out.println("Inventory");
-                    // TODO
-                } else if (tokens[0].equals("list")) {
-                    String student = tokens[1];
-                    System.out.println("List: " + student);
-                    // TODO
-                } else if (tokens[0].equals("exit")) {
-                    // TODO
-                    break;
+        while (sc.hasNextLine()) {
+            String cmd = sc.nextLine();
+            String[] tokens = cmd.split(" ");
+
+            if (tokens[0].equals("setmode")) {
+                char mode = tokens[1].charAt(0);
+                System.out.println("SetMode: " + mode);
+
+                if (mode == 'T') {
+                    sendData("setmode|T");
+                    writer.println(getData());
+                    tcpSocket = new Socket(hostAddress, tcpPort);
+                    tcp = true;
+                } else if (mode == 'U') {
+                    sendData("setmode|U");
+                    writer.println(getData());
+                    tcpSocket.close();
+                    tcp = false;
                 } else {
-                    System.out.println("ERROR: No such command");
+                    System.out.println("Invalid mode");
                 }
+            } else if (tokens[0].equals("borrow")) {
+                String student = tokens[1];
+                String book = cmd.substring(cmd.indexOf('"') - 1);
+                System.out.println("Borrow: Student=" + student +
+                                   " Book=" + book);
+                // TODO
+            } else if (tokens[0].equals("return")) {
+                int id = Integer.parseInt(tokens[1]);
+                System.out.println("Return: " + id);
+                // TODO
+            } else if (tokens[0].equals("inventory")) {
+                System.out.println("Inventory");
+                // TODO
+            } else if (tokens[0].equals("list")) {
+                String student = tokens[1];
+                System.out.println("List: " + student);
+                // TODO
+            } else if (tokens[0].equals("exit")) {
+                // TODO
+                break;
+            } else {
+                System.out.println("ERROR: No such command");
             }
-        } catch (FileNotFoundException e) { e.printStackTrace(); }
+        }
     }
 }
